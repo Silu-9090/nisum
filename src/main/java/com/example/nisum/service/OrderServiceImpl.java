34aed048cddaba17eee23b5a8ea6255d4a315e5b
Silu-9090 <bisoigyana1996@gmail.com>
2024-06-03 16:52:16 +0530

    optimized with stream api

--- src/main/java/com/example/nisum/service/OrderServiceImpl.java
+++ src/main/java/com/example/nisum/service/OrderServiceImpl.java
@@ -4,11 +4,9 @@ import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.chrono.ChronoLocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -22,115 +20,100 @@ import com.example.nisum.model.ResponseItem;
 import com.example.nisum.repository.OfferRepository;
 
 @Service
-public class OrderServiceImpl implements OrderService{
-	
-	@Autowired
-	private OfferRepository offerRepository;
+public class OrderServiceImpl implements OrderService {
+    
+    @Autowired
+    private OfferRepository offerRepository;
 
-	@Override
-	public OrderResponse saveOrder(OrderRequest request) {
-		
-		OrderResponse orderResponse = new OrderResponse();
-		List<ResponseItem> items = new ArrayList<>();
-		
-		List<Offer> offers = offerRepository.findByOfferIdIn(request.getOffers());
-		
-		for(Item item : request.getItems()) {
-			
-			for(String offerId : request.getOffers()) {
-				
-				Optional<Offer> offerOpt = offers.stream()
-	                    .filter(offer -> offer.getOfferId().equals(offerId))
-	                    .findFirst();
+    @Override
+    public OrderResponse saveOrder(OrderRequest request) {
 
-	            if (offerOpt.isPresent()) {
-	                ResponseItem responseItem = validateAndApplyOffer(item, offerOpt.get());
-	                items.add(responseItem);
-	            }
-			}
-		}
-		
-		double totalOrderAmount = items.stream()
-				.mapToDouble(item -> (item.getQty() * item.getUnitPrice()) - item.getDiscount().getAmount())
-				.sum();
-				
-		orderResponse.setOrderNumber(request.getOrderNumber());
-		orderResponse.setItems(items);
-		orderResponse.setOrderTotal(totalOrderAmount);
-		orderResponse.setPromoCode(request.getPromoCode());
-		
-		return orderResponse; 
-	}
+        List<Offer> offers = offerRepository.findByOfferIdIn(request.getOffers());
+        List<ResponseItem> items = request.getItems().stream()
+                .flatMap(item -> request.getOffers().stream()
+                        .map(offerId -> offers.stream()
+                                .filter(offer -> offer.getOfferId().equals(offerId))
+                                .findFirst()
+                                .map(offer -> validateAndApplyOffer(item, offer))
+                                .orElseGet(() -> createResponseItemWithoutDiscount(item))
+                        )
+                )
+                .collect(Collectors.toList());
 
-	private ResponseItem validateAndApplyOffer(Item item, Offer offer) {
-	  if (validateOrder(item, offer)) {
-            return applyDiscount(item, offer);
-      } else {
-    	  
-    	ResponseItem responseItem = new ResponseItem();
-  		OrderDiscount discount = new OrderDiscount();
-  		discount.setAmount(0);
-  		discount.setOffersApplied(Collections.EMPTY_LIST);
-  		
-  		responseItem.setItemCode(item.getItemCode());
-  		responseItem.setProductGroupName(item.getProductGroupName());
-  		responseItem.setQty(item.getQty());
-  		responseItem.setUnitPrice(item.getUnitPrice());
-  		responseItem.setDiscount(discount);
-  		
-  		return responseItem;
-      } 
-	}
+        double totalOrderAmount = items.stream()
+                .mapToDouble(item -> (item.getQty() * item.getUnitPrice()) - item.getDiscount().getAmount())
+                .sum();
 
-	private ResponseItem applyDiscount(Item item, Offer offer) {
-		ResponseItem responseItem = new ResponseItem();
-		OrderDiscount discount = new OrderDiscount();
-		discount.setAmount(offer.getBenefit().getDiscount().getDiscountProducts().getDiscountValue());
-		discount.setOffersApplied(Arrays.asList(offer.getOfferId()));
-		
-		responseItem.setItemCode(item.getItemCode());
-		responseItem.setProductGroupName(item.getProductGroupName());
-		responseItem.setQty(item.getQty());
-		responseItem.setUnitPrice(item.getUnitPrice());
-		responseItem.setDiscount(discount);
-		
-		return responseItem;
-	}
+        OrderResponse orderResponse = new OrderResponse();
+        orderResponse.setOrderNumber(request.getOrderNumber());
+        orderResponse.setItems(items);
+        orderResponse.setOrderTotal(totalOrderAmount);
+        orderResponse.setPromoCode(request.getPromoCode());
 
-	private boolean validateOrder(Item item, Offer offer) {
-		
-		LocalDateTime orderDate = LocalDateTime.now(); 
-		
+        return orderResponse;
+    }
+
+    private ResponseItem validateAndApplyOffer(Item item, Offer offer) {
+        if (validateOrder(item, offer)) {
+            return createResponseItemWithDiscount(item, offer);
+        } else {
+            return createResponseItemWithoutDiscount(item);
+        }
+    }
+
+    private ResponseItem createResponseItemWithDiscount(Item item, Offer offer) {
+        ResponseItem responseItem = new ResponseItem();
+        OrderDiscount discount = new OrderDiscount();
+        discount.setAmount(offer.getBenefit().getDiscount().getDiscountProducts().getDiscountValue());
+        discount.setOffersApplied(Collections.singletonList(offer.getOfferId()));
+
+        responseItem.setItemCode(item.getItemCode());
+        responseItem.setProductGroupName(item.getProductGroupName());
+        responseItem.setQty(item.getQty());
+        responseItem.setUnitPrice(item.getUnitPrice());
+        responseItem.setDiscount(discount);
+
+        return responseItem;
+    }
+
+    private boolean validateOrder(Item item, Offer offer) {
+
+        LocalDateTime orderDate = LocalDateTime.now();
+
         ChronoLocalDateTime<?> offerStartDate = convertStringToChronoLocalDateTime(offer.getOfferStartDate());
         ChronoLocalDateTime<?> offerEndDate = convertStringToChronoLocalDateTime(offer.getOfferEndDate());
-        
+
         if (orderDate.isBefore(offerStartDate) || orderDate.isAfter(offerEndDate)) {
             return false;
         }
-        
-        if(!item
-        		.getProductGroupName()
-        		.equals(offer.getConditions()
-        		.getProductConditions()
-        		.getProductGroupName())
-          ) {
-        	return false;
-        }
 
-
-        double totalAmount = item.getQty() * item.getUnitPrice();
-        
-        if (totalAmount < offer.getConditions().getProductConditions().getMinPurchaseAmt()) {
+        if (!item.getProductGroupName().equals(offer.getConditions().getProductConditions().getProductGroupName())) {
             return false;
         }
 
-        return true;
-	}
+        double totalAmount = item.getQty() * item.getUnitPrice();
 
-	private ChronoLocalDateTime<?> convertStringToChronoLocalDateTime(String date) {
-		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
+        return totalAmount >= offer.getConditions().getProductConditions().getMinPurchaseAmt();
+    }
+
+    private ChronoLocalDateTime<?> convertStringToChronoLocalDateTime(String date) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX");
         ZonedDateTime zonedDateTime = ZonedDateTime.parse(date, formatter);
         return zonedDateTime.toLocalDateTime();
-	}
+    }
 
+    private ResponseItem createResponseItemWithoutDiscount(Item item) {
+        ResponseItem responseItem = new ResponseItem();
+        OrderDiscount discount = new OrderDiscount();
+        discount.setAmount(0);
+        discount.setOffersApplied(Collections.emptyList());
+
+        responseItem.setItemCode(item.getItemCode());
+        responseItem.setProductGroupName(item.getProductGroupName());
+        responseItem.setQty(item.getQty());
+        responseItem.setUnitPrice(item.getUnitPrice());
+        responseItem.setDiscount(discount);
+
+        return responseItem;
+    }
 }
